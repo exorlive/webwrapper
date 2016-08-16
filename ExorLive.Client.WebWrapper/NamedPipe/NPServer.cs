f0989c956a7629ac565187b1436c89aa6bba4457
Tom A. Vibeto <tomandre@exorlive.com>
2016-08-16 11:39:31 +0200

    Fixed bug and upped version number.

--- ExorLive.Client.WebWrapper/NamedPipe/NPServer.cs
+++ ExorLive.Client.WebWrapper/NamedPipe/NPServer.cs
@@ -21,10 +21,7 @@ namespace ExorLive.Client.WebWrapper.NamedPipe
 			_window = window;
 		}
 
-        public string Pipename
-        {
-            get { return "exorlivepipe." + Process.GetCurrentProcess().Id; }
-        }
+		private string Pipename => "exorlivepipe." + Process.GetCurrentProcess().Id;
 
 		public void StartNpServer()
 		{
@@ -54,7 +51,7 @@ namespace ExorLive.Client.WebWrapper.NamedPipe
                     pipeSecurity.AddAccessRule(rule);
 
                     var pipeServer = new NamedPipeServerStream(Pipename, PipeDirection.InOut, 1,
-						PipeTransmissionMode.Byte, PipeOptions.WriteThrough, 1024, 1024, pipeSecurity, HandleInheritability.None, PipeAccessRights.ReadWrite);
+						PipeTransmissionMode.Byte, PipeOptions.WriteThrough, 1024, 1024, pipeSecurity, HandleInheritability.None);
 					_pipeServer = pipeServer;
 					_pipeServer.WaitForConnection(); // This is a blocking call until a client connects.
 					if (!_app.ExorLiveIsRunning)
