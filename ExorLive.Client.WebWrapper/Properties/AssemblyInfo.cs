710cb28034258929a8cca5b631444b26f7137f96
Tom A. Vibeto <tomandre@exorlive.com>
2022-09-21 15:09:43 +0200

    Updated version numbers.

--- ExorLive.Client.WebWrapper/Properties/AssemblyInfo.cs
+++ ExorLive.Client.WebWrapper/Properties/AssemblyInfo.cs
@@ -47,6 +47,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.*")]
+[assembly: AssemblyVersion("2.4.*")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
